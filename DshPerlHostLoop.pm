ac94c0856e0a484abc51be9f179645953554d930
Al Tobey <tobert@gmail.com>
2011-01-11 17:49:17 -0800

    Add function docs & -m option.
    Signed-off-by: Al Tobey <tobert@gmail.com>

--- DshPerlHostLoop.pm
+++ DshPerlHostLoop.pm
@@ -3,7 +3,7 @@ package DshPerlHostLoop;
 ###########################################################################
 #                                                                         #
 # Cluster Tools: DshPerlHostLoop.pm                                       #
-# Copyright 2007-2010, Albert P. Tobey <tobert@gmail.com>                 #
+# Copyright 2007-2011, Albert P. Tobey <tobert@gmail.com>                 #
 #                                                                         #
 ###########################################################################
 
@@ -18,21 +18,21 @@ use IO::Handle;
 use base 'Exporter';
 
 BEGIN {
-    use vars qw( @opt_filter_excl @opt_filter_incl $host_group );
+    use vars qw( @opt_filter_excl @opt_filter_incl );
 }
 
 our $ssh_options = "-o 'BatchMode yes' -o 'StrictHostKeyChecking no' -o 'ConnectTimeout 10'";
 our $tag_output;
 our %ssh2_connections;
-our $debug = undef;
 our $verbose;
+our $machinelist;
 
 # Most shops have a noisy /etc/issue.net. This reads the local issue.net
 # and removes any lines matching it from the output from ssh.
 my @issue = read_issue();
 my $issue_len = length(join("\n", @issue));
 
-our @EXPORT = qw(func_loop ssh scp hostlist reap $ssh_options tag_output @opt_filter_excl @opt_filter_incl $host_group verbose);
+our @EXPORT = qw(func_loop ssh scp hostlist reap $ssh_options tag_output @opt_filter_excl @opt_filter_incl verbose);
 
 =head1 NAME
 
@@ -44,6 +44,20 @@ DshPerlHostLoop - loops for running ssh commands across large clusters in parall
  require "$Bin/DshPerlHostLoop.pm";
 
  func_loop( sub { system( "ssh $_[1] uname -a" ); } );
+
+=head1 GLOBAL SWITCHES
+
+A few global CLI switches are implemented in this module in a BEGIN block.
+
+ --incl - a perl regular expression that filters out non-matching hostnames
+ --excl - a perl regular expression that filters matched hostnames out of the list
+ -t     - tag output with the hostname
+ -v     - verbose
+ -m     - specify a file with a list of hosts to use (default is ~/.dsh/machines.list)
+
+--excl RE's are run before --incl RE's.
+
+=cut
 
 =head1 FUNCTIONS
 
@@ -86,6 +100,15 @@ sub func_loop {
 	reap( \%pids );
 }
 
+=item read_issue()
+
+Read /etc/issue.net or if that doesn't exist, /etc/issue.   This is used to filter out
+issues from remote systems to keep your output readable.
+
+Returns an array of chomped lines.
+
+=cut
+
 sub read_issue {
     my @issue;
     my $fh;
@@ -105,6 +128,34 @@ sub read_issue {
     return @issue;
 }
 
+=item ssh()
+
+Run a command over ssh.
+
+ func_loop(sub {
+     my $host = shift;
+     ssh( $host, 'ps -ef' );
+ });
+
+=item scp()
+
+scp a file.
+
+ my($local_file, $remote_file) = ("/etc/hosts", "/etc/hosts");
+ func_loop(sub {
+     my $host = shift;
+     scp( $local_file, "$host:$remote_file" );
+ });
+
+=item scmd()
+
+The actual function behind ssh/scp.
+
+ sub ssh { scmd('/usr/bin/ssh', @_) }
+ sub scp { scmd('/usr/bin/scp', '-v', @_) }
+
+=cut
+
 # archaic & insecure but fast and convenient
 # in other words, don't let untrusted people sudo this!!
 sub ssh { scmd('/usr/bin/ssh', @_) }
@@ -119,9 +170,6 @@ sub scmd {
     if ( $verbose ) {
         print STDERR "Command($$): $scmd $ssh_options @_\n";
     }
-
-    #print "IN: $in, OUT: $out, ERR: $err\n";
-    #exit;
 
     my $select = IO::Select->new( $out, $err );
     my $ofd = fileno($out);
@@ -157,7 +205,7 @@ sub scmd {
                 $eofs{$rfd} = 1 if ( eof($err) );
             }
             else {
-                warn "Got read on $rfd ...";
+                warn "Got read error on $rfd ...";
             }
 
         }
@@ -175,9 +223,22 @@ sub scmd {
     return @output;
 }
 
+=item hostlist()
+
+Returns an array of hosts to be accessed.  This reads the hostname list (-m $file or default ~/.dsh/machines.list)
+then filters it based on --excl and --incl regular expressions.  
+
+=cut
+
 sub hostlist {
 	my @hostlist;
-    open( my $fh, "< $ENV{HOME}/.dsh/machines.list" );
+
+    if (not defined $machinelist or length $machinelist == 0) {
+        $machinelist = "$ENV{HOME}/.dsh/machines.list";
+    }
+
+    open( my $fh, "< $machinelist")
+        or die "Could not open machine list file '$machinelist' for reading: $!";
 
     HOST: while ( my $hostname = <$fh> ) {
         chomp $hostname;
@@ -187,13 +248,13 @@ sub hostlist {
 
         FILTER_EX: foreach my $excl ( @opt_filter_excl ) {
 		    if ( $hostname =~ /$excl/ ) {
-			    print "DshPerlHostLoop: Skipping $hostname because it matched filter $excl.\n" if ( $debug );
+			    print "DshPerlHostLoop: Skipping $hostname because it matched filter $excl.\n" if ( $verbose );
 			    next HOST;
 		    }
         }
         FILTER_IN: foreach my $incl ( @opt_filter_incl ) {
 		    if ( $hostname !~ /$incl/i ) {
-			    print "DshPerlHostLoop: Skipping $hostname because it didn't match filter $incl.\n" if ( $debug );
+			    print "DshPerlHostLoop: Skipping $hostname because it didn't match filter $incl.\n" if ( $verbose );
 			    next HOST;
 		    }
         }
@@ -206,6 +267,14 @@ sub hostlist {
 	return @hostlist;
 }
 
+=item reap()
+
+Reap child processes from the forkbomb. The hash is { $hostname => $pid }.
+
+ reap(\%pids);
+
+=cut
+
 sub reap {
 	my $pids = shift;
 	foreach my $host ( keys %$pids ) {
@@ -214,12 +283,24 @@ sub reap {
 	}
 }
 
+=item tag_output()
+
+Toggle/get whether or not output should be prefixed with the hostname.
+
+=cut
+
 sub tag_output {
 	if ( @_ == 1 ) {
 		$tag_output = shift;
 	}
 	return $tag_output;
 }
+
+=item verbose()
+
+Toggle/get whether or not to be verbose.
+
+=cut
 
 sub verbose {
     if ( @_ == 1 ) {
@@ -249,13 +330,13 @@ BEGIN {
 			my $f = $main::ARGV[$i+1];
 			push @opt_filter_excl, qr/$f/;
 		}
+		if ( $main::ARGV[$i] eq '-m' ) {
+			push @to_kill, $i, $i+1;
+            $machinelist = $main::ARGV[$i+1];
+		}
 		if ( $main::ARGV[$i] eq '-t' ) {
 			push @to_kill, $i;
 			$tag_output = 1;
-		}
-		if ( $main::ARGV[$i] eq '-g' ) {
-			push @to_kill, $i, $i+1;
-            $host_group = $main::ARGV[$i+1];
 		}
 		if ( $main::ARGV[$i] eq '-v' ) {
 			push @to_kill, $i;
@@ -275,9 +356,11 @@ BEGIN {
 
 __END__
 
+=back
+
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2007-2010 by Al Tobey.
+This software is copyright (c) 2007-2011 by Al Tobey.
 
 This is free software; you can redistribute it and/or modify it under the terms
 of the Artistic License 2.0.  (Note that, unlike the Artistic License 1.0,
